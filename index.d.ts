fbc91cd9b0b6b9d0b481ba23c9585dae11ecbcf3
Angel Zlatanov <38349591+angelzbg@users.noreply.github.com>
2025-08-26 05:32:25 +0300

    fix: add index.d.ts for IDE autocomplete support

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export { createVortexClient, Vortex } from './src/vortex.js';
+export { VortexError } from './src/error.js';
+export { RequestBuilder } from './src/requestBuilder.js';
+export { SimpleCache, NoOpCache, CacheInterface } from './src/cache.js';
